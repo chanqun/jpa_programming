9d991d0bc034e0824dce7a97784199a173d3a1a8
chanqun <cksgns93@naver.com>
2021-07-18 13:59:57 +0900

    조회 API 컨트롤러 개발

--- querydsl/src/main/kotlin/com/example/querydsl/controller/MemberController.kt
+++ querydsl/src/main/kotlin/com/example/querydsl/controller/MemberController.kt
@@ -1,12 +1,20 @@
 package com.example.querydsl.controller
 
+import com.example.querydsl.dto.MemberSearchCondition
+import com.example.querydsl.dto.MemberTeamDto
+import com.example.querydsl.repository.MemberJpaRepository
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class MemberController {
 
-//    @Autowired
-//    lateinit var memberJpaRepository: Me
+    @Autowired
+    lateinit var memberJpaRepository: MemberJpaRepository
+
+    @GetMapping("/v1/members")
+    fun funName(searchCondition: MemberSearchCondition): List<MemberTeamDto> {
+        return memberJpaRepository.search(searchCondition)
+    }
 }
